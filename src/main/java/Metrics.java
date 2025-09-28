86fb26ed63d2bde2ca6375aefb17eb2ed74f42d0
littlebithard <lenovolegionvers15@gmail.com>
2025-09-28 21:17:45 +0500

    Update Metrics.java

--- src/main/java/Metrics.java
+++ src/main/java/Metrics.java
@@ -1,22 +1,49 @@
-public final class Metrics {
-    public long comparisons;
-    public long swaps;
-    public long allocations;
+import java.util.concurrent.atomic.LongAdder;
+
+public class Metrics {
+    public final String algorithmName;
+    public long executionTimeNanos;
+    private final LongAdder comparisons = new LongAdder();
+    private int maxRecursionDepth = 0;
     private int currentDepth = 0;
-    public int maxDepth = 0;
-    public long startTime;
-    public long endTime;
+
+    public Metrics(String algorithmName) {
+        this.algorithmName = algorithmName;
+    }
+
+    public void incrementComparisons() {
+        comparisons.increment();
+    }
 
     public void enterRecursion() {
         currentDepth++;
-        if (currentDepth > maxDepth) maxDepth = currentDepth;
+        if (currentDepth > maxRecursionDepth) {
+            maxRecursionDepth = currentDepth;
+        }
     }
 
     public void exitRecursion() {
         currentDepth--;
     }
 
-    public void startTimer() { startTime = System.nanoTime(); }
-    public void stopTimer() { endTime = System.nanoTime(); }
-    public double elapsedMillis() { return (endTime - startTime) / 1_000_000.0; }
+    public void reset() {
+        comparisons.reset();
+        maxRecursionDepth = 0;
+        currentDepth = 0;
+        executionTimeNanos = 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "Algorithm: %s\n" +
+            "  - Execution Time: %.4f ms\n" +
+            "  - Comparisons: %,d\n" +
+            "  - Max Recursion Depth: %d",
+            algorithmName,
+            executionTimeNanos / 1_000_000.0,
+            comparisons.longValue(),
+            maxRecursionDepth
+        );
+    }
 }
